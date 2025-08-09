2ea031b19dc496d8c950fe7a41b2f79dbc548959
Cristiano Pacheco <chris.spb25@gmail.com>
2025-08-09 14:53:55 -0300

    refactor: change function parameters to use pointers for router and handler in auth and user setup routes

--- internal/modules/user/http/fiber/router/user_router.go
+++ internal/modules/user/http/fiber/router/user_router.go
@@ -5,7 +5,7 @@ import (
 	"github.com/cristiano-pacheco/pingo/internal/shared/modules/http/router"
 )
 
-func SetupUserRoutes(router router.Router, handler handler.UserHandler) {
+func SetupUserRoutes(router *router.Router, handler *handler.UserHandler) {
 	r := router.Router()
 	r.Post("/users", handler.CreateUser)
 	r.Put("/users", handler.UpdateUser)
