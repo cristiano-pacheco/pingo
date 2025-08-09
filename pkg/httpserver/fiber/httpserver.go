1fe1b73b314ea17d128fb51dca2a00641766996f
Cristiano Pacheco <chris.spb25@gmail.com>
2025-08-09 13:48:57 -0300

    fix: update Swagger handler and remove unused middleware parameter in Fiber HTTP server

--- pkg/httpserver/fiber/httpserver.go
+++ pkg/httpserver/fiber/httpserver.go
@@ -4,14 +4,13 @@ import (
 	"context"
 	"fmt"
 
-	// ...existing code...
 	"time"
 
 	"github.com/gofiber/contrib/otelfiber"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
-	httpSwagger "github.com/swaggo/fiber-swagger"
+	fiberSwagger "github.com/swaggo/fiber-swagger"
 
 	_ "github.com/cristiano-pacheco/pingo/docs" // imports swagger docs for API documentation
 )
@@ -36,8 +35,9 @@ func NewFiberHTTPServer(
 	app := fiber.New()
 	app.Use(recover.New())
 	app.Use(cors.New(corsConfig))
+
 	if isOtelEnabled {
-		app.Use(otelfiber.Middleware(otelHandlerName))
+		app.Use(otelfiber.Middleware())
 	}
 	// Health check
 	app.Get("/healthcheck", func(c *fiber.Ctx) error {
@@ -46,7 +46,7 @@ func NewFiberHTTPServer(
 	// Metrics endpoint (not implemented: prometheus middleware not available for Fiber v2)
 	// TODO: Add metrics endpoint if needed
 	// Swagger
-	app.Get("/swagger/*", httpSwagger.Handler())
+	app.Get("/swagger/*", fiberSwagger.WrapHandler)
 	return &FiberHTTPServer{app: app}
 }
 
