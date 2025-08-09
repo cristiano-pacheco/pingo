7b8031cfa79c9abd2dd0c014e512168338dc5255
Cristiano Pacheco <chris.spb25@gmail.com>
2025-08-09 14:59:43 -0300

    feat: initialize configuration and OpenTelemetry in main function

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "github.com/cristiano-pacheco/pingo/cmd"
+import (
+	"context"
+	"log"
+
+	"github.com/cristiano-pacheco/pingo/cmd"
+	"github.com/cristiano-pacheco/pingo/internal/shared/modules/config"
+	"github.com/cristiano-pacheco/pingo/internal/shared/modules/otel"
+)
 
 func main() {
+	config.Init()
+	cfg := config.GetConfig()
+	otel.Init(cfg)
+
+	defer func() {
+		if err := otel.Trace().Shutdown(context.Background()); err != nil {
+			log.Printf("Error shutting down tracer provider: %v", err)
+		}
+	}()
+
 	cmd.Execute()
 }
