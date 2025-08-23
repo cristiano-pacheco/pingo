1023fc2fbc404b916a2341698809078e28db8967
Cristiano Pacheco <chris.spb25@gmail.com>
2025-08-23 18:28:44 -0300

    refactor: update UserActivateUseCase to validate user status and improve token handling

--- internal/modules/identity/usecase/user_activate_usecase.go
+++ internal/modules/identity/usecase/user_activate_usecase.go
@@ -16,21 +16,29 @@ import (
 )
 
 type UserActivateInput struct {
-	Token string `validate:"required"`
+	UserID uint64 `validate:"required"`
+	Token  string `validate:"required"`
 }
 
 type UserActivateUseCase struct {
-	userRepository repository.UserRepository
-	validate       validator.Validate
-	logger         logger.Logger
+	oneTimeTokenRepository repository.OneTimeTokenRepository
+	userRepository         repository.UserRepository
+	validate               validator.Validate
+	logger                 logger.Logger
 }
 
 func NewUserActivateUseCase(
+	oneTimeTokenRepository repository.OneTimeTokenRepository,
 	userRepository repository.UserRepository,
 	validate validator.Validate,
 	logger logger.Logger,
 ) *UserActivateUseCase {
-	return &UserActivateUseCase{userRepository, validate, logger}
+	return &UserActivateUseCase{
+		oneTimeTokenRepository: oneTimeTokenRepository,
+		userRepository:         userRepository,
+		validate:               validate,
+		logger:                 logger,
+	}
 }
 
 func (uc *UserActivateUseCase) Execute(ctx context.Context, input UserActivateInput) error {
@@ -48,25 +56,45 @@ func (uc *UserActivateUseCase) Execute(ctx context.Context, input UserActivateIn
 		return err
 	}
 
-	user, err := uc.userRepository.FindPendingConfirmation(ctx, token)
+	user, err := uc.userRepository.FindByID(ctx, input.UserID)
 	if err != nil && !errors.Is(err, shared_errs.ErrRecordNotFound) {
 		uc.logger.Error().
 			Msgf("Failed to find user with confirmation token for the user_id: %d, error: %v", user.ID, err)
 		return err
 	}
 
-	if user.ID == 0 {
+	if user.Status != enum.UserStatusPending {
+		return errs.ErrUserNotInPendingStatus
+	}
+
+	confirmationTokenType, err := enum.NewTokenTypeEnum(enum.TokenTypeAccountConfirmation)
+	if err != nil {
+		return err
+	}
+
+	oneTimeToken, err := uc.oneTimeTokenRepository.Find(ctx, user.ID, confirmationTokenType)
+	if err != nil {
+		uc.logger.Error().Msgf("Failed to find one-time token for the user_id: %d, error: %v", user.ID, err)
+		return err
+	}
+
+	if oneTimeToken.ID == 0 || string(oneTimeToken.TokenHash) != string(token) {
 		return errs.ErrInvalidAccountConfirmationToken
 	}
 
 	now := time.Now().UTC()
 	user.ConfirmedAt = &now
 	user.UpdatedAt = now
-	user.ConfirmationToken = []byte{}
 	user.Status = enum.UserStatusActive
 	err = uc.userRepository.Update(ctx, user)
 	if err != nil {
 		uc.logger.Error().Msgf("Failed to update user confirmation status for the user_id: %d, error %v", user.ID, err)
+		return err
+	}
+
+	err = uc.oneTimeTokenRepository.Delete(ctx, user.ID, confirmationTokenType)
+	if err != nil {
+		uc.logger.Error().Msgf("Failed to delete one-time token for the user_id: %d, error: %v", user.ID, err)
 		return err
 	}
 
