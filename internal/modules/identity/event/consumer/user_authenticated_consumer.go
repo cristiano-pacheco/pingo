464f93ec0ff6596b509d85e88a6da308a3cd9266
Cristiano Pacheco <chris.spb25@gmail.com>
2025-09-02 20:48:41 -0300

    feat: implement UserAuthenticatedConsumer and refactor AuthLoginUseCase to produce user authenticated messages

---
+++ internal/modules/identity/event/consumer/user_authenticated_consumer.go
@@ -0,0 +1,120 @@
+package consumer
+
+import (
+	"context"
+	"crypto/rand"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/cristiano-pacheco/pingo/internal/modules/identity/enum"
+	"github.com/cristiano-pacheco/pingo/internal/modules/identity/event"
+	"github.com/cristiano-pacheco/pingo/internal/modules/identity/model"
+	"github.com/cristiano-pacheco/pingo/internal/modules/identity/repository"
+	"github.com/cristiano-pacheco/pingo/internal/modules/identity/service"
+	"github.com/cristiano-pacheco/pingo/internal/shared/errs"
+	"github.com/cristiano-pacheco/pingo/internal/shared/modules/logger"
+	"github.com/cristiano-pacheco/pingo/internal/shared/modules/otel"
+	"github.com/cristiano-pacheco/pingo/pkg/kafka"
+)
+
+const (
+	verificationCodeTTL = 10 * time.Minute
+	maxRandomNumber     = 1000000
+)
+
+type UserAuthenticatedConsumer struct {
+	sendEmailVerificationCodeService service.SendEmailVerificationCodeService
+	oneTimeTokenRepository           repository.OneTimeTokenRepository
+	userRepository                   repository.UserRepository
+	hashService                      service.HashService
+	logger                           logger.Logger
+	otel                             otel.Otel
+}
+
+func NewUserAuthenticatedConsumer(
+	sendEmailVerificationCodeService service.SendEmailVerificationCodeService,
+	oneTimeTokenRepository repository.OneTimeTokenRepository,
+	userRepository repository.UserRepository,
+	hashService service.HashService,
+	logger logger.Logger,
+	otel otel.Otel,
+) *UserAuthenticatedConsumer {
+	return &UserAuthenticatedConsumer{
+		oneTimeTokenRepository:           oneTimeTokenRepository,
+		userRepository:                   userRepository,
+		hashService:                      hashService,
+		sendEmailVerificationCodeService: sendEmailVerificationCodeService,
+		logger:                           logger,
+		otel:                             otel,
+	}
+}
+
+func (c *UserAuthenticatedConsumer) Topic() string {
+	return event.IdentityUserAuthenticatedTopic
+}
+
+func (c *UserAuthenticatedConsumer) GroupID() string {
+	return "default"
+}
+
+func (c *UserAuthenticatedConsumer) ProcessMessage(ctx context.Context, message kafka.Message) error {
+	ctx, span := c.otel.StartSpan(ctx, "AuthLoginUseCase.Execute")
+	defer span.End()
+
+	var userAuthenticatedMessage event.UserAuthenticatedMessage
+	if err := json.Unmarshal(message.Value, &userAuthenticatedMessage); err != nil {
+		c.logger.Error().Msgf("error unmarshaling message: %v", err)
+		return err
+	}
+
+	user, err := c.userRepository.FindByID(ctx, userAuthenticatedMessage.UserID)
+	if err != nil {
+		c.logger.Error().Msgf("error finding user by ID %d: %v", userAuthenticatedMessage.UserID, err)
+		return err
+	}
+
+	emailVerificationType, _ := enum.NewTokenTypeEnum(enum.TokenTypeLoginVerification)
+	if err = c.oneTimeTokenRepository.Delete(ctx, user.ID, emailVerificationType); err != nil &&
+		!errors.Is(err, errs.ErrRecordNotFound) {
+		c.logger.Error().Msgf("error deleting verification codes for user ID %d: %v", user.ID, err)
+		return err
+	}
+
+	n, err := rand.Int(rand.Reader, big.NewInt(maxRandomNumber))
+	if err != nil {
+		c.logger.Error().Msgf("error generating verification code: %v", err)
+		return err
+	}
+
+	code := fmt.Sprintf("%06d", n.Int64())
+	tokenHash, err := c.hashService.GenerateFromPassword([]byte(code))
+	if err != nil {
+		c.logger.Error().Msgf("error hashing verification code for user ID %d: %v", user.ID, err)
+		return err
+	}
+
+	tokenType, _ := enum.NewTokenTypeEnum(enum.TokenTypeLoginVerification)
+	oneTimeToken := model.OneTimeTokenModel{
+		UserID:    user.ID,
+		TokenHash: tokenHash,
+		TokenType: tokenType.String(),
+		ExpiresAt: time.Now().UTC().Add(verificationCodeTTL),
+		CreatedAt: time.Now().UTC(),
+	}
+
+	if _, err = c.oneTimeTokenRepository.Create(ctx, oneTimeToken); err != nil {
+		c.logger.Error().Msgf("error creating one-time token for user ID %d: %v", user.ID, err)
+		return err
+	}
+
+	sendEmailVerificationCodeInput := service.SendEmailVerificationCodeInput{UserID: user.ID, Code: code}
+	if err = c.sendEmailVerificationCodeService.Execute(ctx, sendEmailVerificationCodeInput); err != nil {
+		c.logger.Error().Msgf("error sending verification code email for user ID %d: %v", user.ID, err)
+		return err
+	}
+
+	return nil
+}
--- internal/modules/identity/usecase/auth_login_usecase.go
+++ internal/modules/identity/usecase/auth_login_usecase.go
@@ -2,26 +2,18 @@ package usecase
 
 import (
 	"context"
-	"crypto/rand"
 	"errors"
-	"fmt"
-	"math/big"
-	"time"
 
 	"github.com/cristiano-pacheco/pingo/internal/modules/identity/enum"
 	"github.com/cristiano-pacheco/pingo/internal/modules/identity/errs"
-	"github.com/cristiano-pacheco/pingo/internal/modules/identity/model"
+	"github.com/cristiano-pacheco/pingo/internal/modules/identity/event"
+	"github.com/cristiano-pacheco/pingo/internal/modules/identity/event/producer"
 	"github.com/cristiano-pacheco/pingo/internal/modules/identity/repository"
 	"github.com/cristiano-pacheco/pingo/internal/modules/identity/service"
 	shared_errs "github.com/cristiano-pacheco/pingo/internal/shared/errs"
 	"github.com/cristiano-pacheco/pingo/internal/shared/modules/logger"
 	"github.com/cristiano-pacheco/pingo/internal/shared/modules/otel"
 	"github.com/cristiano-pacheco/pingo/internal/shared/modules/validator"
-)
-
-const (
-	verificationCodeTTL = 10 * time.Minute
-	maxRandomNumber     = 1000000
 )
 
 type AuthLoginInput struct {
@@ -34,32 +26,29 @@ type AuthLoginOutput struct {
 }
 
 type AuthLoginUseCase struct {
-	oneTimeTokenRepository           repository.OneTimeTokenRepository
-	userRepository                   repository.UserRepository
-	validator                        validator.Validate
-	hashService                      service.HashService
-	sendEmailVerificationCodeService service.SendEmailVerificationCodeService
-	logger                           logger.Logger
-	otel                             otel.Otel
+	userAuthenticatedProducer producer.UserAuthenticatedProducer
+	userRepository            repository.UserRepository
+	hashService               service.HashService
+	validator                 validator.Validate
+	logger                    logger.Logger
+	otel                      otel.Otel
 }
 
 func NewAuthLoginUseCase(
-	oneTimeTokenRepository repository.OneTimeTokenRepository,
+	userAuthenticatedProducer producer.UserAuthenticatedProducer,
 	userRepository repository.UserRepository,
 	validator validator.Validate,
 	hashService service.HashService,
-	sendEmailVerificationCodeService service.SendEmailVerificationCodeService,
 	logger logger.Logger,
 	otel otel.Otel,
 ) *AuthLoginUseCase {
 	return &AuthLoginUseCase{
-		userRepository:                   userRepository,
-		oneTimeTokenRepository:           oneTimeTokenRepository,
-		validator:                        validator,
-		hashService:                      hashService,
-		sendEmailVerificationCodeService: sendEmailVerificationCodeService,
-		logger:                           logger,
-		otel:                             otel,
+		userAuthenticatedProducer: userAuthenticatedProducer,
+		userRepository:            userRepository,
+		validator:                 validator,
+		hashService:               hashService,
+		logger:                    logger,
+		otel:                      otel,
 	}
 }
 
@@ -70,72 +59,30 @@ func (u *AuthLoginUseCase) Execute(ctx context.Context, input AuthLoginInput) (A
 		return AuthLoginOutput{}, err
 	}
 
-	user, err := u.findAndValidateUser(ctx, input)
+	user, err := u.userRepository.FindByEmail(ctx, input.Email)
+	if err != nil && !errors.Is(err, shared_errs.ErrRecordNotFound) {
+		u.logger.Error().Msgf("error finding by email %v", err)
+		return AuthLoginOutput{}, err
+	}
+
+	if user.ID == 0 {
+		return AuthLoginOutput{}, errs.ErrInvalidCredentials
+	}
+
+	if user.Status != enum.UserStatusActive {
+		return AuthLoginOutput{}, errs.ErrUserIsNotActive
+	}
+
+	if err = u.hashService.CompareHashAndPassword(user.PasswordHash, []byte(input.Password)); err != nil {
+		return AuthLoginOutput{}, errs.ErrInvalidCredentials
+	}
+
+	message := event.UserAuthenticatedMessage{UserID: user.ID}
+	err = u.userAuthenticatedProducer.Produce(ctx, message)
 	if err != nil {
-		return AuthLoginOutput{}, err
-	}
-
-	emailVerificationType, _ := enum.NewTokenTypeEnum(enum.TokenTypeLoginVerification)
-	if err = u.oneTimeTokenRepository.Delete(ctx, user.ID, emailVerificationType); err != nil &&
-		!errors.Is(err, shared_errs.ErrRecordNotFound) {
-		u.logger.Error().Msgf("error deleting verification codes for user ID %d: %v", user.ID, err)
-		return AuthLoginOutput{}, err
-	}
-
-	n, err := rand.Int(rand.Reader, big.NewInt(maxRandomNumber))
-	if err != nil {
-		u.logger.Error().Msgf("error generating verification code: %v", err)
-		return AuthLoginOutput{}, err
-	}
-
-	code := fmt.Sprintf("%06d", n.Int64())
-	tokenHash, err := u.hashService.GenerateFromPassword([]byte(code))
-	if err != nil {
-		u.logger.Error().Msgf("error hashing verification code for user ID %d: %v", user.ID, err)
-		return AuthLoginOutput{}, err
-	}
-
-	tokenType, _ := enum.NewTokenTypeEnum(enum.TokenTypeLoginVerification)
-	oneTimeToken := model.OneTimeTokenModel{
-		UserID:    user.ID,
-		TokenHash: tokenHash,
-		TokenType: tokenType.String(),
-		ExpiresAt: time.Now().UTC().Add(verificationCodeTTL),
-		CreatedAt: time.Now().UTC(),
-	}
-
-	if _, err = u.oneTimeTokenRepository.Create(ctx, oneTimeToken); err != nil {
-		u.logger.Error().Msgf("error creating one-time token for user ID %d: %v", user.ID, err)
-		return AuthLoginOutput{}, err
-	}
-
-	sendEmailVerificationCodeInput := service.SendEmailVerificationCodeInput{UserID: user.ID, Code: code}
-	if err = u.sendEmailVerificationCodeService.Execute(ctx, sendEmailVerificationCodeInput); err != nil {
-		u.logger.Error().Msgf("error sending verification code email for user ID %d: %v", user.ID, err)
+		u.logger.Error().Msgf("error producing user authenticated message for user id: %v, error: %v", user.ID, err)
 		return AuthLoginOutput{}, err
 	}
 
 	return AuthLoginOutput{UserID: user.ID}, nil
-}
-
-func (u *AuthLoginUseCase) findAndValidateUser(ctx context.Context, input AuthLoginInput) (model.UserModel, error) {
-	user, err := u.userRepository.FindByEmail(ctx, input.Email)
-	if err != nil && !errors.Is(err, shared_errs.ErrRecordNotFound) {
-		u.logger.Error().Msgf("error finding by email %v", err)
-		return model.UserModel{}, err
-	}
-
-	if user.ID == 0 {
-		return model.UserModel{}, errs.ErrInvalidCredentials
-	}
-
-	if user.Status != enum.UserStatusActive {
-		return model.UserModel{}, errs.ErrUserIsNotActive
-	}
-
-	if err = u.hashService.CompareHashAndPassword(user.PasswordHash, []byte(input.Password)); err != nil {
-		return model.UserModel{}, errs.ErrInvalidCredentials
-	}
-
-	return user, nil
 }
